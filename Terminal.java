e14c79e169394e7846b5378cd3d6a8c88cb640c4
AB <bera1107@usherbrooke.ca>
2022-07-09 10:58:56 -0400

    Squelette
    code fourni dans la section labo de la problematiquer

---
+++ Terminal.java
@@ -0,0 +1,22 @@
+package app6;
+
+/** @author Ahmed Khoumsi */
+
+/** Cette classe identifie les terminaux reconnus et retournes par
+ *  l'analyseur lexical
+ */
+public class Terminal {
+
+
+// Constantes et attributs
+//  ....
+
+
+/** Un ou deux constructeurs (ou plus, si vous voulez)
+  *   pour l'initalisation d'attributs 
+ */	
+  public Terminal( ) {   // arguments possibles
+     //
+  }
+
+}
